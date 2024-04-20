447d9c2f26aa35f5d59d8c4e70d2eea72fa3202d
Max <55982815+TheDemon2179@users.noreply.github.com>
2024-04-20 16:27:00 +0300

    oopsie

--- comparator.py
+++ comparator.py
@@ -1,18 +1,24 @@
 import keyboard
-import comparator_module
+import comparator
 from PIL import ImageGrab
 
 def paste_image():
-    image = ImageGrab.grabclipboard() # i find it quite convenient
+    image = ImageGrab.grabclipboard()
     
     if image is not None:
-        image.save("pasted_image.png")
-        print("Изображение успешно вставлено и сохранено.")
-        print("\nНачинается поиск.")
-        comparator_module.compare_image_with_database('pasted_image.png')
+        if isinstance(image, list):
+            for i, img in enumerate(image):
+                img.save(f"pasted_image_{i}.png")
+                print(f"Изображение {i} успешно вставлено и сохранено.")
+                print("\nНачинается поиск.")
+                comparator.compare_image_with_database(f'pasted_image_{i}.png')
+        else:
+            image.save("pasted_image.png")
+            print("Изображение успешно вставлено и сохранено.")
+            print("\nНачинается поиск.")
+            comparator.compare_image_with_database('pasted_image.png')
     else:
         print("В буфере обмена нет изображения.")
 
 keyboard.add_hotkey('ctrl+v', paste_image) # lots of false inputs if running in the background, if it disturbs you, fix it yourself xD
-
 keyboard.wait()
